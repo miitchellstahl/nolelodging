fad428dad485489875172b6a6166a66af29c62a8
Mitchell Stahl <mitchellstahl@Mitchells-MacBook-Pro.local>
2022-10-07 01:07:40 -0400

    server-setup3

--- app.js
+++ app.js
@@ -2,12 +2,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
-require("dotenv").config({ path: "./config.env" });
+require("dotenv").config({ path: "/config.env" });
 const methodOverride = require("method-override");
-const Dorm = require("./models/dorm");
+const Dorm = require("/models/dorm");
 const dormInfoArray = require("./seeds/dormInfoArray");
-const catchAsync = require("./utils/catchAsync");
-const ExpressError = require("./utils/ExpressError");
+const catchAsync = require("/utils/catchAsync");
+const ExpressError = require("/utils/ExpressError");
 
 //Connect to database
 const MONGOURI = process.env.MONGO_URI;
